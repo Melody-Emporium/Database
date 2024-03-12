e1a603365fadf99a1078d84afe321f8a88b04afe
Víctor López <vicrenlopez14115@gmail.com>
2024-03-12 15:16:12 -0600

    Corrections

--- melody-emporium-c.sql
+++ melody-emporium-c.sql
@@ -3,59 +3,71 @@ CREATE DATABASE IF NOT EXISTS melody_emporium;
 USE melody_emporium;
 
 -- Usuario para la base de datos
-CREATE USER 'emporioso_1'@'%' IDENTIFIED WITH mysql_native_password BY 'melodia_emporiosa';
+# CREATE USER 'emporioso_1'@'%' IDENTIFIED WITH mysql_native_password BY 'melodia_emporiosa';
 
 -- Asignar permisos DML
-GRANT INSERT, UPDATE, DELETE, SELECT ON melody_emporium.* TO 'emporioso_1'@'%' WITH GRANT OPTION;
+# GRANT INSERT, UPDATE, DELETE, SELECT ON melody_emporium.* TO 'emporioso_1'@'%' WITH GRANT OPTION;
 
 -- Asignar permisos para ejecutar y crear funciones, procedimientos, triggers y vistas
-GRANT PROCESS, EXECUTE, CREATE ROUTINE, ALTER ROUTINE, CREATE VIEW ON melody_emporium.* TO 'emporioso_1'@'%';
+# GRANT PROCESS, EXECUTE, CREATE ROUTINE, ALTER ROUTINE, CREATE VIEW ON melody_emporium.* TO 'emporioso_1'@'%';
 
 
 -- Create genres table
 CREATE TABLE genres
 (
-    genre_id    INT PRIMARY KEY NOT NULL,
-    name        VARCHAR(255)    NOT NULL,
+    genre_id    CHAR(36) PRIMARY KEY NOT NULL,
+    name        VARCHAR(255)         NOT NULL,
     picture_url TINYTEXT
 );
+
+ALTER TABLE genres
+    ALTER genre_id SET DEFAULT (UUID());
 
 -- Create artists table
 CREATE TABLE artists
 (
-    artist_id   INT PRIMARY KEY NOT NULL,
-    name        VARCHAR(255)    NOT NULL,
+    artist_id   CHAR(36) PRIMARY KEY NOT NULL,
+    name        VARCHAR(255)         NOT NULL,
     picture_url TINYTEXT
 );
+
+ALTER TABLE artists
+    ALTER artist_id SET DEFAULT (UUID());
 
 -- Create product_categories table
 CREATE TABLE product_categories
 (
-    product_category_id INT PRIMARY KEY NOT NULL,
-    name                VARCHAR(255)    NOT NULL,
-    description         TINYTEXT        NOT NULL,
+    product_category_id CHAR(36) PRIMARY KEY NOT NULL,
+    name                VARCHAR(255)         NOT NULL,
+    description         TINYTEXT             NOT NULL,
     color               VARCHAR(6) DEFAULT 'ECD0C0',
     picture_url         TINYTEXT
 );
 
+ALTER TABLE product_categories
+    ALTER product_category_id SET DEFAULT (UUID());
+
 -- Create products table
 CREATE TABLE products
 (
-    product_id            INT PRIMARY KEY NOT NULL,
-    name                  TINYTEXT        NOT NULL,
-    short_description     LONGTEXT        NOT NULL,
-    details               LONGTEXT        NOT NULL,
-    features              LONGTEXT        NOT NULL,
-    picture_url           TINYTEXT        NOT NULL,
-    price                 DECIMAL(10, 2)  NOT NULL DEFAULT 0.0,
-    promo_price           DECIMAL(10, 2)  NULL     DEFAULT 0.0,
-    units_in_stock        INT             NOT NULL DEFAULT 0,
-    product_category_id   INT,
-    genre_id              INT,
-    artist_id             INT,
+    product_id            CHAR(36) PRIMARY KEY NOT NULL,
+    name                  TINYTEXT             NOT NULL,
+    short_description     LONGTEXT             NOT NULL,
+    details               LONGTEXT             NOT NULL,
+    features              LONGTEXT             NOT NULL,
+    picture_url           TINYTEXT             NOT NULL,
+    price                 DECIMAL(10, 2)       NOT NULL DEFAULT 0.0,
+    promo_price           DECIMAL(10, 2)       NULL     DEFAULT 0.0,
+    units_in_stock        INT                  NOT NULL DEFAULT 0,
+    product_category_id   CHAR(36),
+    genre_id              CHAR(36),
+    artist_id             CHAR(36),
     primary_variant_price DECIMAL(10, 2) UNSIGNED,
-    rating_stars          TINYINT         NOT NULL DEFAULT 0
+    rating_stars          TINYINT              NOT NULL DEFAULT 0
 );
+
+ALTER TABLE products
+    ALTER product_id SET DEFAULT (UUID());
 
 -- Add foreign key constraints
 ALTER TABLE products
@@ -73,22 +85,35 @@ ALTER TABLE products
 -- Create users table
 CREATE TABLE users
 (
-    user_id     INT PRIMARY KEY NOT NULL,
-    name        VARCHAR(255)    NOT NULL,
-    email       VARCHAR(255)    NOT NULL,
-    pass        VARCHAR(255)    NOT NULL,
-    picture_url TINYTEXT        NOT NULL,
-    is_admin    BOOLEAN         NOT NULL DEFAULT 0,
-    is_active   BOOLEAN         NOT NULL DEFAULT 1
+    user_id     CHAR(36) PRIMARY KEY NOT NULL,
+    name        VARCHAR(255)         NOT NULL,
+    username    VARCHAR(255)         NOT NULL,
+    email       VARCHAR(255)         NOT NULL,
+    pass        VARCHAR(255)         NOT NULL,
+    picture_url TINYTEXT             NOT NULL,
+    is_admin    BOOLEAN              NOT NULL DEFAULT 0,
+    is_active   BOOLEAN              NOT NULL DEFAULT 1
 );
+
+ALTER TABLE users
+    ADD UNIQUE (username);
+
+ALTER TABLE users
+    ALTER user_id SET DEFAULT (UUID());
+
+ALTER TABLE users
+    ADD UNIQUE (email);
 
 -- Create carts table
 CREATE TABLE carts
 (
-    cart_id  INT PRIMARY KEY NOT NULL,
-    user_id  INT             NOT NULL,
-    subtotal DECIMAL(10, 2)  NOT NULL DEFAULT 0.0
+    cart_id  CHAR(36) PRIMARY KEY NOT NULL,
+    user_id  CHAR(36)             NOT NULL,
+    subtotal DECIMAL(10, 2)       NOT NULL DEFAULT 0.0
 );
+
+ALTER TABLE carts
+    ALTER cart_id SET DEFAULT (UUID());
 
 -- Add foreign key constraint
 ALTER TABLE carts
@@ -99,13 +124,16 @@ ALTER TABLE carts
 -- Create cart_items table
 CREATE TABLE cart_items
 (
-    cart_item  INT PRIMARY KEY         NOT NULL,
-    product_id INT                     NOT NULL,
-    cart_id    INT                     NOT NULL,
+    cart_item  CHAR(36) PRIMARY KEY    NOT NULL,
+    product_id CHAR(36)                NOT NULL,
+    cart_id    CHAR(36)                NOT NULL,
     unit_price DECIMAL(10, 2) UNSIGNED NOT NULL,
     amount     DECIMAL(10, 2) UNSIGNED NOT NULL,
     quantity   INT                     NOT NULL
 );
+
+ALTER TABLE cart_items
+    ALTER cart_item SET DEFAULT (UUID());
 
 -- Add foreign key constraints
 ALTER TABLE cart_items
@@ -120,13 +148,16 @@ ALTER TABLE cart_items
 -- Create client_ratings table
 CREATE TABLE client_ratings
 (
-    client_rating_id INT PRIMARY KEY NOT NULL,
-    stars_given      TINYINT         NOT NULL,
+    client_rating_id CHAR(36) PRIMARY KEY NOT NULL,
+    stars_given      TINYINT              NOT NULL,
     comments         TINYTEXT,
-    date_recorded    DATE            NOT NULL,
-    product_id       INT,
-    user_id          INT             NOT NULL
+    date_recorded    DATE                 NOT NULL,
+    product_id       CHAR(36),
+    user_id          CHAR(36)             NOT NULL
 );
+
+ALTER TABLE client_ratings
+    ALTER client_rating_id SET DEFAULT (UUID());
 
 -- Add foreign key constraints
 ALTER TABLE client_ratings
